2488389b9455aceaef83e8b73ee07a2c59693e4d
Lucas Pontes Arruda <paluke94@gmail.com>
2022-03-26 10:52:34 -0300

    [fix] router imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Login } />
+      <Routes>
+        <Route exact path='/' element={ <Login /> } />
         {/* <Route path="/search" component={ Search } />
         <Route path="/album/:id" component={ Album } />
         <Route path="/favorites" component={ Favorites } />
         <Route exact path="/profile" component={ Profile } />
         <Route path="/profile/edit" component={ ProfileEdit } />
         <Route path="*" component={ NotFound } /> */}
-      </Switch>
+      </Routes>
     </BrowserRouter>
   );
 }
