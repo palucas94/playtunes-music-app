e05905dc847b797c557f2d27c2381d76094ace19
Lucas Pontes Arruda <paluke94@gmail.com>
2022-03-26 12:59:31 -0300

    [feat] search page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Search from './pages/Search';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={ <Login /> } />
-        {/* <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
+        <Route path="/search" element={ <Search /> } />
+        {/* <Route path="/album/:id" component={ Album } />
         <Route path="/favorites" component={ Favorites } />
         <Route exact path="/profile" component={ Profile } />
         <Route path="/profile/edit" component={ ProfileEdit } />
---
+++ src/pages/Search.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import Albums from '../components/Albums';
+import Header from '../components/Header';
+import tunesAPI from '../services/tunesAPI';
+
+function Search() {
+  const [artist, setArtist] = useState('');
+  const [albums, setAlbums] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const inputHandler = ({ target: { value } }) => {
+    setArtist(value);
+  };
+
+  const buttonHandler = async () => {
+    setLoading(true);
+    const results = await tunesAPI(artist);
+    console.log(results);
+    setAlbums(results);
+    setLoading(false);
+  };
+
+  return (
+    <div className="search-container">
+      <Header />
+      <div className="form-container">
+        <form>
+          <input
+            type="text"
+            className="seach-input"
+            value={ artist }
+            onChange={ inputHandler }
+          />
+          <button
+            type='button'
+            className="search-button"
+            onClick={ buttonHandler }
+          >
+            Search
+          </button>
+        </form>
+      </div>
+      <div className="albums-container">
+        <div className="albums-wrapper">
+          { albums.length > 0 && <Albums collection={ albums } artist={ artist }  />}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
