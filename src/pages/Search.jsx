2da0685ec5556058949a0f7428c2a913ecd76b63
Lucas Pontes Arruda <paluke94@gmail.com>
2022-03-27 12:28:28 -0300

    [feat] conditional rendering in Search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Albums from '../components/Albums';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 import { tunesAPI } from '../services/tunesAPI';
 
 function Search() {
   const [artist, setArtist] = useState('');
+  const [currentArtist, setCurrentArtist] = useState('');
   const [albums, setAlbums] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -15,33 +17,58 @@ function Search() {
   const buttonHandler = async () => {
     setLoading(true);
     const results = await tunesAPI(artist);
+    setCurrentArtist(artist);
     setAlbums(results);
     setLoading(false);
   };
 
+  if (loading) {
+    return (
+      <div>
+        <Header />
+        <form className="search-form">
+        <input
+          placeholder='Search for artist...'
+          type="text"
+          className="search-input"
+          value={ artist }
+          onChange={ inputHandler }
+        />
+        <button
+          type='button'
+          className="search-button"
+          onClick={ buttonHandler }
+        >
+          Search
+        </button>
+      </form>
+        <Loading />
+      </div>
+    );
+  }
+
   return (
     <div className="search-container">
       <Header />
-      <div className="form-container">
-        <form>
-          <input
-            type="text"
-            className="seach-input"
-            value={ artist }
-            onChange={ inputHandler }
-          />
-          <button
-            type='button'
-            className="search-button"
-            onClick={ buttonHandler }
-          >
-            Search
-          </button>
-        </form>
-      </div>
+      <form className="search-form">
+        <input
+          placeholder='Search for artist...'
+          type="text"
+          className="search-input"
+          value={ artist }
+          onChange={ inputHandler }
+        />
+        <button
+          type='button'
+          className="search-button"
+          onClick={ buttonHandler }
+        >
+          Search
+        </button>
+      </form>
       <div className="albums-container">
         <div className="albums-wrapper">
-          { albums.length > 0 && <Albums collection={ albums } artist={ artist }  />}
+          { albums.length > 0 && <Albums collection={ albums } artist={ currentArtist }  />}
         </div>
       </div>
     </div>
