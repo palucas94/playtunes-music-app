3ad0a7737ab54b64986a656c892b5ab129f9bcb4
Lucas Pontes Arruda <paluke94@gmail.com>
2022-03-27 07:41:20 -0300

    [feat] add EditProfile page

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Search from './pages/Search';
 import Album from './pages/Album';
 import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
+import EditProfile from './pages/EditProfile';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={ <Login /> } />
-        <Route path="/search" element={ <Search /> } />
-        <Route path="/album/:id" element={ < Album /> } />
-        <Route path="/profile" element={ <Profile /> } />
+        <Route path='/search' element={ <Search /> } />
+        <Route path='/album/:id' element={ < Album /> } />
+        <Route path='/profile' element={ <Profile /> } />
+        <Route path='/profile/edit' element={ <EditProfile /> } />
         <Route path="*" element={ <NotFound /> } />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/EditProfile.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import Header from '../components/Header';
+import { saveUser } from '../services/user';
+
+function EditProfile() {
+  const [newName, setNewName] = useState('');
+  const [newEmail, setNewEmail] = useState('');
+  const [newImg, setNewImg] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  const inputHandler = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'img':
+        setNewImg(value);
+        break;
+      case 'email':
+        setNewEmail(value);
+        break;
+      case 'userName':
+        setNewName(value);
+        break;
+      default:
+        break; 
+    };
+  }
+  
+  const submitHandler = () => {
+    saveUser({ name: newName, email: newEmail, img: newImg });
+    setRedirect(true);
+  };
+
+  if (redirect) return <Navigate to='/profile' />;
+
+  return (
+    <div className="edit-profile-container">
+      <Header />
+      <form onSubmit={ submitHandler }>
+        <img src='' alt='User' />
+        <input
+          name='img'
+          value={ newImg }
+          onChange={ inputHandler }
+          placeholder='Insert a link'
+          type="text"
+          className="img-input"
+        />
+        <h3>Name</h3>
+        <input
+          name='userName'
+          value={ newName }
+          onChange={ inputHandler }
+          placeholder='Your name here'
+          type="text"
+          className="name-input"
+        />
+        <h3>Email</h3>
+        <input
+          name='email'
+          value={ newEmail }
+          onChange={ inputHandler }
+          placeholder='user@user.com'
+          type="text"
+          className="name-input"
+        />
+        <button type='submit'>Save</button>
+      </form>
+    </div>
+  );
+}
+
+export default EditProfile;
